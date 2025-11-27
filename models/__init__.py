49f40efa66510ddd66249e32a62db373d4e08b16
ZackFred07 <Zgame0077@gmail.com>
2025-11-26 21:31:42 -0500

    Cleanup broken diffuLLaMa stuff

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 from .LLaDA import LLaDA
 from .LLaDA_DAEDAL import LLaDA_DAEDAL
-from .DiffuLLaMa import DiffuLLaMA
-from .DiffuLLaMA_DAEDAL import DiffuLLaMA_DAEDAL
+# from .DiffuLLaMa import DiffuLLaMA
+# from .DiffuLLaMA_DAEDAL import DiffuLLaMA_DAEDAL
 from .Dream import Dream
 from .Dream_DAEDAL import Dream_DAEDAL
 
-__all__ = ["LLaDA", "LLaDA_DAEDAL", "DiffuLLaMA", "Dream", "Dream_DAEDAL"]
+__all__ = ["LLaDA", "LLaDA_DAEDAL", "Dream", "Dream_DAEDAL"]
