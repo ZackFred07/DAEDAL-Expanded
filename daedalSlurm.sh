bfb16ad89e015f9843d2f097899aac1d255aaee8
ZackFred07 <Zgame0077@gmail.com>
2025-11-26 21:19:34 -0500

    What my script would look like

--- daedalSlurm.sh
+++ daedalSlurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=16:00:00
+#SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:nvidia_h100_pcie:2
 #SBATCH --account=course_cap6614
-#SBATCH --job-name="DAEDAL1p5Eval"
+#SBATCH --job-name="DAEDAL_DREAM"
 
 # Original Repo Setup
 module load anaconda
@@ -21,12 +21,17 @@ pip install sacrebleu sqlitedict torch==2.1.2 torchvision==0.16.2 torchaudio==2.
 mkdir ckpts
 cd ckpts
 git lfs install
-git clone https://huggingface.co/GSAI-ML/LLaDA-8B-Instruct
-git clone https://huggingface.co/GSAI-ML/LLaDA-1.5
+# git clone https://huggingface.co/GSAI-ML/LLaDA-8B-Instruct
+# git clone https://huggingface.co/GSAI-ML/LLaDA-1.5
+git clone https://huggingface.co/Dream-org/Dream-v0-Instruct-7B
+git clone
 cd ..
 
 # Run Evaluation (Choose Which One You Need and Comment Out What You Don't)
-sh scripts/eval_LLaDA_1p5_DAEDAL.sh
-sh scripts/eval_LLaDA_1p5_Baseline.sh
-sh scripts/eval_LLaDA_DAEDAL.sh
-sh scripts/eval_LLaDA_Baseline.sh
+# sh scripts/eval_LLaDA_1p5_DAEDAL.sh
+# sh scripts/eval_LLaDA_1p5_Baseline.sh
+# sh scripts/eval_LLaDA_DAEDAL.sh
+# sh scripts/eval_LLaDA_Baseline.sh
+# sh scripts/eval_LLaMA_8B_Baseline.sh
+sh scripts/eval_Dream_Baseline.sh
+sh scripts/eval_Dream_DAEDAL.sh
