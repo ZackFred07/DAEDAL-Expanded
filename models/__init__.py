2059aac84b04527c0f29aa3c8ebe73bab97c1509
ZackFred07 <Zgame0077@gmail.com>
2025-11-30 10:47:07 -0500

    Logarithmic Search DAEDAL

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 from .LLaDA import LLaDA
 from .LLaDA_DAEDAL import LLaDA_DAEDAL
+from .LLaDA_LDAEDAL import LLaDA_LDAEDAL
 # from .DiffuLLaMa import DiffuLLaMA
 # from .DiffuLLaMA_DAEDAL import DiffuLLaMA_DAEDAL
 from .Dream import Dream
 from .Dream_DAEDAL import Dream_DAEDAL
 
-__all__ = ["LLaDA", "LLaDA_DAEDAL", "Dream", "Dream_DAEDAL"]
+__all__ = ["LLaDA", "LLaDA_DAEDAL", "Dream", "Dream_DAEDAL", "LLaDA_LDAEDAL"]
